cab251f2eaface7b30f9e99d86b61411b8904024
PapayaOL <kban0208@gmail.com>
2022-05-27 23:51:14 -0400

    Character select screen

---
+++ Assets/CharacterSelect.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSelect : MonoBehaviour
+{
+    public void SelectChar(int charNum) { 
+        
+    }
+}
